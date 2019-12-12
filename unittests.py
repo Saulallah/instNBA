70355213543eec1888d4e9231a182ca3443b3f03
Saulallah <50159178+Saulallah@users.noreply.github.com>
2019-12-11 22:56:51 -0500

    Add files via upload

---
+++ unittests.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#Name: Sulayman Sallah
+#Directory ID: 114724113
+#Date: December 09, 2019
+#Assignment: Data unittests
+
+import unittest
+from 
+
+class TestData(unittest.TestCase):
+
+    def test_data(self):
+        self.assertEqual(nba['Age'].count(), 501), "Should be 501"
+        self.assertEqual(nba['USG%'].count(), 501), "Should be 501"
+if __name__ == '__main__':
+    unittest.main()
+
+
+# In[ ]:
+
+
+
+
